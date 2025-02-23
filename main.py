979d7ebc05b9684a2a52f64c94d965411cb0a8ab
Andre-Edwards <andreedwardswork@gmail.com>
2025-02-23 14:31:37 +0700

    added 2 more books, all tests passed

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from stats import get_word_count, get_characters, get_book_text
+import sys
 def main():
-    book_path = "books/frankenstein.txt"
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    
+    book_path = sys.argv[1]
     text = get_book_text(book_path)
     words = get_word_count(text)
     characters = get_characters(text)
-
 
     print("============ BOOKBOT ============")
     print(F"Analayzing book found at {book_path}...")
